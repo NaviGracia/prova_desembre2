bb643851a7b37fec03bf5df5713925a8bdd5f6c0
NaviGracia <ivagartor2@alu.edu.gva.es>
2022-12-14 09:51:08 +0100

    Prueba mejorada

---
+++ config.js
@@ -0,0 +1,15 @@
+function blau(){
+    var document.getElementById("nom"),
+    
+}
+
+function rosa(){
+    var document.getElementById("adreça")
+}
+
+function suma(){
+    var nom = document.getElementById("nombre").value;
+    var adreça = document.getElementById("dirección")
+    var age = document.getElementById("edat").value;
+    alert ('benvingut' + nom + adreça + ',' + age)
+}
